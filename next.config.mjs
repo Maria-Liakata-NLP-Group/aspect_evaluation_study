f0a24ee526623f0c6d113d3bf425ee6abfc08e97
SFRL <sebastian.lobbers@googlemail.com>
2024-09-23 12:44:04 +0100

    fix config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/nlp_reasoning_annotations" : "",
-  assetPrefix: isProd ? "/nlp_reasoning_annotations/" : "",
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
-  output: "export", // This enables static export in Next.js 14+
 };
 
 export default nextConfig;
