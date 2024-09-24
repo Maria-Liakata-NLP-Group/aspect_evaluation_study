cb381ad2078f52eeb92032a656b316709cbbdd52
SFRL <sebastian.lobbers@googlemail.com>
2024-09-24 17:53:23 +0100

    looks like maybe i didn't push the api call

---
+++ src/pages/api/getData.js
@@ -0,0 +1,20 @@
+import { kv } from "@vercel/kv";
+
+export default async function handler(request, response) {
+  // Get batch id
+  const batchId = await kv.lindex("queue", 1);
+
+  // Get claim ids in batch
+  const claimIdsRequest = await kv.hgetall(batchId);
+  const claimIds = claimIdsRequest.claim_ids
+
+  const claims = await Promise.all(
+    claimIds.map(async (claimId) => {
+      const claim = await kv.hgetall(claimId);
+      claim.id = claimId;
+      return claim;
+    })
+  );
+  
+  return response.status(200).json({"claims": claims, "batchId": batchId});
+}
---
+++ src/pages/api/hello.js
@@ -0,0 +1,4 @@
+// hello world api call
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from Vercel!" });
+}
